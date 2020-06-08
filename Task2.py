a8de5c073541c12283fd6807b13d98ec01baf592
Jadug Parusa <jadug.parusa@shopee.com>
2020-06-08 22:48:02 +0800

    feat: Calculate cumulative durations with dictionary

--- Task2.py
+++ Task2.py
@@ -1,6 +1,9 @@
 """
 Read file into texts and calls.
 It's ok if you don't understand how to read files
+
+text.csv columns: sending number (string), receiving number (string), message timestamp (string).
+call.csv columns: calling number (string), receiving number (string), start timestamp (string), duration in seconds (string)
 """
 import csv
 with open('texts.csv', 'r') as f:
@@ -19,4 +22,23 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+max_duration = 0
+max_duration_number = ""
+duration_dict = dict()
 
+for call_record in calls:
+    # calculate callers duration = new + existing duration
+    total_duration = int(call_record[3]) + duration_dict.get(call_record[0], 0)
+    duration_dict[call_record[0]] = total_duration
+    if max_duration < total_duration:
+        max_duration_number = call_record[0]
+        max_duration = total_duration
+    
+    # calculate receivers duration = new + existing duration
+    total_duration = int(call_record[3]) + duration_dict.get(call_record[1], 0)
+    duration_dict[call_record[1]] = total_duration
+    if max_duration < total_duration:
+        max_duration_number = call_record[1]
+        max_duration = total_duration
+
+print("{0} spent the longest time, {1} seconds, on the phone during September 2016.".format(max_duration_number, max_duration))
